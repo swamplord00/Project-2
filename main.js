ecd9cd1868e1424908331de8afb8854bc631d255
Victor Briones <vbrionesparra@gmail.com>
2023-04-20 19:23:14 -0400

    styles tarjetero + code cleaning

--- main.js
+++ main.js
@@ -1,42 +1,7 @@
-//Arreglo de objetos opinion ejemplo
 
-// const opiniones=[
-//     {
-//         estrellas:4,
-//         producto:"Crema cicatrizante",
-//         review:"Excelentes resultados, la aplique sobre una cicatriz antigua y he tenido buenos resultados.",
-//         autor:"Scar",
-
-//     }
-// ]
 
 // Obtener Contenedor Opiniones
 const contenedor = document.getElementById("contenedorOpiniones")
-//Tarjeta de ejemplo
-// opiniones.forEach((el)=>{
-//     contenedor.innerHTML+=`<article class="registro" id=${el.codigo}>
-//     <div class=new-star-rating>
-//         <a href="#" class="strY">★</a>
-//         <a href="#" class="strY">★</a>
-//         <a href="#" class="strY">★</a>
-//         <a href="#" class="strY">★</a>
-//         <a href="#" class="strG">★</a>     
-//     </div>
-//     <div class="post">
-//         <h3> Producto: ${el.producto}</h3>
-//         <p>${el.review}</p>
-//         <h4>Firma: ${el.autor}</h4>
-//     </div>
-//     <div class="opciones">
-//         <button class=editBtn>Editar</button>
-//         <button class=deleteBtn>Eliminar</button>
-//     </div>
-// </article>`
-// })
-
-
-
-
 
 //  seleccionar Imagen principal del producto
 
@@ -70,96 +35,7 @@ const setSelectImg = () => {
 formlistProduct.addEventListener('change', () => setSelectImg())
 
 
-// //CREAR tarjeta de opinion de ejemplo nueva_______________________
 
-// const tarjeta = document.createElement('article')
-
-// tarjeta.className = 'registro'
-// tarjeta.id = 'nueva'
-
-// // Crear div de valoracion
-// const stars = document.createElement('div')
-// stars.className = 'new-star-rating'
-
-
-// let contYstars = 4
-// let contGstar = 5 - contYstars
-
-// for (let i = 0; i < 5; i++) {
-//     let anchor = document.createElement('a')
-//     anchor.src = '#'
-//     anchor.innerText = '★'
-//     if (i < contYstars) {
-//         anchor.className = 'strY'
-//     } else {
-//         anchor.className = 'strG'
-//     }
-//     stars.appendChild(anchor)
-// }
-
-// // Crear div de post
-
-// const post = document.createElement('div')
-// post.className = 'post'
-
-// const productName = document.createElement('h3')
-// productName.innerText = 'Producto:'
-// const productReview = document.createElement('p')
-// productReview.innerText = 'reseña de producto'
-// const sign = document.createElement('h4')
-// sign.innerText = 'firma:'
-
-// post.appendChild(productName)
-// post.appendChild(productReview)
-// post.appendChild(sign)
-
-// // Crear acciones 
-
-// const acciones = document.createElement('div')
-// acciones.className = 'opciones'
-
-// const btnEdit = document.createElement('button')
-// btnEdit.className = 'editBtn'
-// btnEdit.innerText = 'Editar'
-
-// const btnDelete = document.createElement('button')
-// btnDelete.className = 'deleteBtn'
-// btnDelete.innerText = 'Eliminar'
-
-// acciones.appendChild(btnEdit)
-// acciones.appendChild(btnDelete)
-
-
-// tarjeta.appendChild(stars)
-// tarjeta.appendChild(post)
-// tarjeta.appendChild(acciones)
-// contenedor.appendChild(tarjeta)
-
-// Seleccionar inputs formulario
-// let starsValue=0;
-
-// const formrReview=document.getElementById('opinion')
-// // const formRadios=document.getElementsByName('str')
-// const formRadios1=document.getElementById('1str')
-// const formRadios2=document.getElementById('2str')
-// const formRadios3=document.getElementById('3str')
-// const formRadios4=document.getElementById('4str')
-// const formRadios5=document.getElementById('5str')
-// const formSign=document.getElementById('firma')
-
-
-// // Crear EventListener
-
-// formRadios1.addEventListener('input',handlerInput)
-// formRadios2.addEventListener('input',handlerInput)
-// formRadios3.addEventListener('input',handlerInput)
-// formRadios4.addEventListener('input',handlerInput)
-// formRadios5.addEventListener('input',handlerInput)
-// formlistProduct.addEventListener('input',handlerInput)
-// formrReview.addEventListener('input',handlerInput)
-// formSign.addEventListener('input',handlerInput)
-
-// Variables de estado Global
 
 let idEditing=null
 let modeEdit=false
@@ -176,6 +52,7 @@ ids.forEach((id) => {
 let reviewsArrays = []
 cargarArray()
 crearTarj()
+
 function cargarArray(){
     
     const registro=JSON.parse(localStorage.getItem('reviews'))
@@ -187,10 +64,6 @@ function cargarArray(){
 
 }
 
-function eliminarRegistroLS(){
-
-}
-
 function pushRegistroLS(){
     localStorage.setItem('reviews', JSON.stringify(reviewsArrays))
 
@@ -199,7 +72,7 @@ function pushRegistroLS(){
 // Crear objeto
 let reviewObject = {}
 function handlerInput(event) {
-    // console.log(event.target.value)
+    
     const { value, name } = event.target
     reviewObject = {
         ...reviewObject,
@@ -216,7 +89,7 @@ btnAdd.addEventListener('click', agregarReview)
 
 function agregarReview(evento) {
     evento.preventDefault();
-    alert("hicieron click")
+    
     console.log(Object.values(reviewObject))
     
     if (validarForm(reviewObject)) {
@@ -239,7 +112,7 @@ function agregarReview(evento) {
 //Validar form
 
 function validarForm(Nreview) {
-    // const {dropdownlist,review,str, sign}=Nreview
+    
     let obj=Object.entries(Nreview)
     if(obj.length<3 ){
         return false
@@ -360,6 +233,8 @@ function editarFila(id){
     
 }
 
+// Actualizar reviews
+
 document.getElementById('edit').addEventListener('click',actualizar)
 
 function actualizar(event){
@@ -392,6 +267,8 @@ function actualizar(event){
 
 }
 
+// Deshabilitar botones de tarjetero
+
 function disableBtns(){
     const btns= document.querySelectorAll('.formBtn')
     console.log(btns)
@@ -407,6 +284,7 @@ function disableBtns(){
     
 }
 
+// Enfocar elementos en  modo ediciòn
 function focusCard(){
 
     const card=document.getElementById(idEditing)
